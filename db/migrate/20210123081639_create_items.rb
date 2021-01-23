13feafda5dbb2f348e73b678f9ef8ddb22737193
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-01-23 19:14:20 +0900

    add_migrate_items

---
+++ db/migrate/20210123081639_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      # t.references :seller, null: false, foreign_key: { to_table: :users }
+      # t.references :buyer, foreign_key: { to_table: :users }
+      t.integer :trading_status, null: false
+      t.text :introduction, null: false
+      # t.references :category, null: false, foreign_key: true
+      t.integer :item_condition, null: false
+      t.integer :shipping_fee_payer, null: false
+      t.integer :prefecture, null: false
+      t.integer :preparation_day, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
