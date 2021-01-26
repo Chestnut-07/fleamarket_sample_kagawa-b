5bb54488a68c9b1aa675bf37fda1af3c25659fbf
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-01-26 19:27:38 +0900

    add_create_items.rb_references_category

--- db/migrate/20210123081639_create_items.rb
+++ db/migrate/20210123081639_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.references :buyer, foreign_key: { to_table: :users }
       t.integer :trading_status, null: false
       t.text :introduction, null: false
-      # t.references :category, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.integer :item_condition, null: false
       t.integer :shipping_fee_payer, null: false
       t.integer :prefecture, null: false
