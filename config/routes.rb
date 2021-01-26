00b30b5e2a0dafb6a32b14486ec80844109c4cf5
yamakyou <yyyxkyo10@i.softbank.jp>
2021-01-26 10:11:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :items, only: [:index, :new, :show]
 end
