0a8023d94c870a88f41719b1c776fd9014a7623e
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-01-27 18:23:27 +0900

    add_shipping_fee_payer.rb_name

--- app/models/shipping_fee_payer.rb
+++ app/models/shipping_fee_payer.rb
@@ -1,8 +1,8 @@
 class ShippingFeePayer < ActiveHash::Base
 
   self.data = [
-    {id: 1, '送料込み(出品者負担)'},
-    {id: 2, '着払い(購入者負担)'}
+    {id: 1, name: '送料込み(出品者負担)'},
+    {id: 2, name: '着払い(購入者負担)'}
   ]
 
   include ActiveHash::Associations
