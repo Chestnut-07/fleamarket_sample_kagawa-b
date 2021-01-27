6d480f02115039048828607faf8485f24e61e973
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-01-27 16:26:53 +0900

    add_model_items.rb_ActiveRecord_ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   has_one :item_image, dependent: :destroy
   belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee_payer
   belongs_to_active_hash :prefecture
