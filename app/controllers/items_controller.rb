f1cf8af55af65732080f42d8ea8955c64a6db450
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-01-29 18:36:43 +0900

    add_items_controller.rb_item_params_strongparams

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,5 +21,7 @@ class ItemsController < ApplicationController
 
   private
 
+  def item_params
+    params.require(:item).permit(:name, :explanation, :categories_id, :item_condition, :shipping_fee_payer_id, :prefecture_id, :preparation_day_id, :price).marge(seller_id: current_user.id)
   end
 end
