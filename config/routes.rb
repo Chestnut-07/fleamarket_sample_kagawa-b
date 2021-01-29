bbcd1751e5351ab2617568d61528b2d462e34729
bonkura-bird <dia8856@gmail.com>
2021-01-29 15:19:04 +0900

    add: index,newルートを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :users, only: [:show]
+  resources :credit_cards, only: [:index, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
