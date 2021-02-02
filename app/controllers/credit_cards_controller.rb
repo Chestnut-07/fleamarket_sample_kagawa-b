67fce301d9b3fea279522785d6331b1ec73354f4
bonkura-bird <dia8856@gmail.com>
2021-02-02 15:51:50 +0900

    有効期限を含む配列のインスタンス変数を生成

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -3,6 +3,11 @@ class CreditCardsController < ApplicationController
   end
 
   def new
+    @exp_year = []
+    11.times do |i|
+      today_year = Date.today.year % 100
+      @exp_year << today_year + i
+    end
   end
 
 end
