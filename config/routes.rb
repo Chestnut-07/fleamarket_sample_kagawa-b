18afd7b02b22d22f2f2076f49e289db6c223b388
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-02 16:59:25 +0900

    fix_routes.rb_resorce_change_resorces

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :items, only: [:index, :new, :show]
+  resources :items, only: [:index, :show, :new]
 end
