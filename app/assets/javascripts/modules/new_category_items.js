f79478f1162e1b15994b15e50ca687200b730eeb
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-06 13:02:54 +0900

    add_new_category_items.js_ajax

--- app/assets/javascripts/modules/new_category_items.js
+++ app/assets/javascripts/modules/new_category_items.js
@@ -4,5 +4,12 @@ $(function(){
     alert('change!')
     $(this).nextAll().remove()
     const category_id = $(this).val()
+
+    $.ajax({
+      type: "GET",
+      url: "api/items/categories",
+      data: {category_id: category_id},
+      dataType: 'json'
+    })
   })
 })
