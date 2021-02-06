590e3e4b063793ae77a62278446b4d743ccdaa59
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-06 12:59:08 +0900

    add_routes.rb_get_get_categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :credit_cards, only: [:index, :new]
   resources :items, only: [:index, :new, :show]
+  get 'api/items/categories', to: 'items#get_categories'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
