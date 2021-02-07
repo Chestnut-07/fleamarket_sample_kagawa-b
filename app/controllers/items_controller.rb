d7331170e7db75288837bca00806e955e3318490
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-07 13:43:28 +0900

    add_controller_items_controller.rb_create_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,4 +18,12 @@ class ItemsController < ApplicationController
     @children = current_category.children
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
