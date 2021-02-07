a0d18cc04f336c3639ab14b07c27598d0c2f2453
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-07 13:39:33 +0900

    delete_model_item.rb_delivery

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,4 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee_payer
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_day
-  belongs_to_active_hash :delivery
 end
