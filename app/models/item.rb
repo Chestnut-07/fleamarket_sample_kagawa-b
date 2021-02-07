5de96947dec094ea06f2ef2043a088dc0786183d
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-07 13:39:55 +0900

    add_model_accepts_nested_attributes_for_item_image

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
   has_one :item_image, dependent: :destroy
+  accepts_nested_attributes_for :item_image, allow_destroy: true
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
