8051b0b3e22f0cc01b78f705d5ec643a59bb371e
bonkura-bird <dia8856@gmail.com>
2021-02-08 14:53:05 +0900

    売却済みの商品一覧を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   def index
     @new_items = Item.all.order("created_at desc").limit(10)
+    @sold_items = Item.where(trading_status: 2).order("created_at desc").limit(10)
+
   end
   
   def new
