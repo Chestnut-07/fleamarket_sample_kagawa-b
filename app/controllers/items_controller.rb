588058631007307c873c32a3e42e970cd28ee43a
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-09 14:54:54 +0900

    delete_items_controller.rb_@category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @category = Category.all
     if @item.save
       redirect_to root_path
     else
