baf08d00db4f5e346d3694ad6e09e514efd118e2
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-09 15:05:54 +0900

    add_new_fee_items.js

---
+++ app/assets/javascripts/modules/new_fee_items.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('#Price').on("change", function(){
+    let countNum = $(this).val();
+    let fee = Math.floor(countNum * 0.1);
+    let profit = countNum - fee;
+    if (countNum >= 300){
+      $('.FormItemPanel__Feevalue').html(fee);
+      $('.FormItemPanel__Totalvalue').html(profit);
+    } else {
+      $('.FormItemPanel__Feevalue').html("ー");
+      $('.FormItemPanel__Totalvalue').html("ー");
+    }
+  });
+});
