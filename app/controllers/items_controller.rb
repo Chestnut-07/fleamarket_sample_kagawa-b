7bfb5fafe81ea9fddf968ea2e553cee664dcdeb8
bonkura-bird <dia8856@gmail.com>
2021-02-09 17:16:17 +0900

    売却済みの商品ではなく、ランダムで１カテゴリーの商品を取得するように変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   def index
     @new_items = Item.all.order("created_at desc").limit(10)
-    @sold_items = Item.where(trading_status: 2).order("created_at desc").limit(10)
+    random_category = Item.all.shuffle.take(1)[0][:category_id]
+    @pickup_items = Item.where(category: random_category, trading_status: 1).shuffle.take(10)
 
   end
   def perchase_confirmation
