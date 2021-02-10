06d2f3a95778487f7d2937fbc0da9b0709e75e9f
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-10 23:17:51 +0900

    add_models_item.rb_validates_price_numericality_only_iteger_greter_than_&_less_than

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 40 }
     validates :introduction, length: { maximum: 1000 }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
