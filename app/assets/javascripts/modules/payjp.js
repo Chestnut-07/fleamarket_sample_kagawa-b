c5c760909d0e310f585971b643a564abe8ca2616
bonkura-bird <dia8856@gmail.com>
2021-02-11 21:03:37 +0900

    js : フォームよりカード情報を取得

--- app/assets/javascripts/modules/payjp.js
+++ app/assets/javascripts/modules/payjp.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", function(){
   Payjp.setPublicKey(gon.payjp_public_key);
+  $("#card_token_submit").on("click", function(e){
+    e.preventDefault();
+    var card = {
+      number: document.getElementById("card_number_form").value,
+      cvc: document.getElementById("cvc_form").value,
+      exp_month: document.getElementById("exp_month_form").value,
+      exp_year: document.getElementById("exp_year_form").value
+    };
+  });
 });
