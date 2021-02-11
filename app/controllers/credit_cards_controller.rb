e5ef4017f365f6e4228f469fe25296a0a2b359e4
bonkura-bird <dia8856@gmail.com>
2021-02-11 22:31:40 +0900

    CreditCardのインスタンスを生成

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -6,6 +6,7 @@ class CreditCardsController < ApplicationController
 
   def new
     gon.payjp_public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @credit_card = CreditCard.new
     @exp_year = []
     11.times do |i|
       today_year = Date.today.year % 100
@@ -15,6 +16,7 @@ class CreditCardsController < ApplicationController
 
   def create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    @credit_card = CreditCard.new(user: current_user, customer_token: customer.id, card_token: customer.default_card)
   end
 
 end
