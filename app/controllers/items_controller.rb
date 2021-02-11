a19e6ba1d7317a75354d431af1f53833e5bbaf1f
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2021-02-11 11:53:01 +0900

    Revert "Merge branch 'master' into add_items_new_server_side"
    This reverts commit 6df11e3f77c9b4c9fb301fb8109710092b9947bc, reversing
    changes made to 527b6b20cc8a1a5bfec6f0ce85ea1d6f88619824.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,6 @@
 class ItemsController < ApplicationController
 
   def index
-    @new_items = Item.all.order("created_at desc").limit(10)
-    random_category = Item.all.shuffle.take(1)[0][:category_id]
-    @pickup_items = Item.where(category: random_category, trading_status: 1).shuffle.take(10)
-
   end
 
   def perchase_confirmation
