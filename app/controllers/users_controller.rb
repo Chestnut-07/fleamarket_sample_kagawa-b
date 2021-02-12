991ca89769fbca865624423bc0db92536cfd4e0c
bonkura-bird <dia8856@gmail.com>
2021-02-12 14:24:34 +0900

    マイページの名前、出品数に符合するインスタンスを生成、挿入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+    @selling_item = Item.where(seller_id: current_user.id, trading_status: 1).count
   end
 
   def new
