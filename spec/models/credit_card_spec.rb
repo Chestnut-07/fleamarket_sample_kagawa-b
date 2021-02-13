97ad33b8971b7921471088c45bd37e945e6a5635
bonkura-bird <dia8856@gmail.com>
2021-02-13 16:26:28 +0900

    test: is valid with all columun 作成

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 describe CreditCard do
   describe '#create' do
+    
+    it "is valid with all columun" do
+      credit_card = build(:credit_card)
+      expect(credit_card).to be_valid
+    end
+
   end
 end
 
