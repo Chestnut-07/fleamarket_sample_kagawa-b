630c36c5a59d73d9846115a7b6ae31a30a15268e
bonkura-bird <dia8856@gmail.com>
2021-02-13 16:39:24 +0900

    credit_cardテーブルの各カラムがnilの際のテストを作成

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -7,6 +7,24 @@ describe CreditCard do
       expect(credit_card).to be_valid
     end
 
+    it "is invalid without a user" do
+      credit_card = build(:credit_card, user: nil)
+      credit_card.valid?
+      expect(credit_card).to be_invalid
+    end
+
+    it "is invalid without a customer_token" do
+      credit_card = build(:credit_card, customer_token: nil)
+      credit_card.valid?
+      expect(credit_card).to be_invalid
+    end
+
+    it "is invalid without a card_token" do
+      credit_card = build(:credit_card, card_token: nil)
+      credit_card.valid?
+      expect(credit_card).to be_invalid
+    end
+
   end
 end
 
