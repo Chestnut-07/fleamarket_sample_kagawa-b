87d63adde6034de36e28cda8245f1de48099c162
bonkura-bird <dia8856@gmail.com>
2021-02-14 13:36:27 +0900

    user_showアクションにログイン状態による分岐を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @selling_item = Item.where(seller_id: current_user.id, trading_status: 1).count
+    if user_signed_in?
+      @user = current_user
+      @selling_item = Item.where(seller_id: current_user.id, trading_status: 1).count
+    else
+      redirect_to root_path, alert: "ログインしてください。"
+    end
   end
 
   def new
