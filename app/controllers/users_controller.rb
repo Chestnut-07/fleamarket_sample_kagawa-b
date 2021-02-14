3339d54bd91321651e155330186d7a15cab1b679
bonkura-bird <dia8856@gmail.com>
2021-02-14 15:45:44 +0900

    不要なendを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @selling_item = Item.where(seller_id: current_user.id, trading_status: 1).count
-    end
   end
 
   def new
